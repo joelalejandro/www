c7a4d120f020536b4121143ddda31e463f51923e
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2020-04-11 00:28:52 -0300

    added favicon

--- src/main.js
+++ src/main.js
@@ -9,5 +9,22 @@ export default function (Vue, { head }) {
   head.link.push({
     rel: 'stylesheet',
     href: 'https://use.typekit.net/bgs4por.css'
-  })
+  }, {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/apple-touch-icon.png'
+  }, {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon-32x32.png'
+  }, {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon-16x16.png'
+  }, {
+    rel: 'manifest',
+    href: '/site.webmanifest'
+  });
 }
