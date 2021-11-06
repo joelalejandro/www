06ce4a7848565c3dadd7e9ae13455b8c9ad4651f
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-11-06 14:05:21 -0300

    fixed menu in mobile

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,7 +13,7 @@
           <p
             class="text-gray-700 text-lg sm:text-3xl italic dark:text-gray-300"
           >Trabajos e ideas de Joel A. Villarreal Bertoldi</p>
-          <nav class="p-6 grid grid-flow-col gap-4">
+          <nav class="p-6 grid grid-flow-row md:grid-flow-col gap-4">
             <g-link to="/talks" class="text-gray-900 hover:bg-gray-300 dark:text-gray-400 border-gray-400 text-lg border rounded-full px-4 py-2 transition duration-300">Charlas</g-link>
             <g-link to="/ir" class="text-gray-900 hover:bg-gray-300 dark:text-gray-400 border-gray-400 text-lg border rounded-full px-4 py-2 transition duration-300">#IngenieríaReversa</g-link>
             <g-link to="/ask-me-anything" class="text-gray-900 hover:bg-gray-300 dark:text-gray-400 border-gray-400 text-lg border rounded-full px-4 py-2 transition duration-300">Preguntame</g-link>
